f8c77193e0893cf85ceeb4c2d33185a9fb4705ed
pragath-pth <pragathpth@gmail.com>
2023-05-20 13:47:12 +0530

    Optimizations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pragath-pth';
 
-  constructor(public router: Router){ }
+  constructor(public router: Router, private meta: Meta){ }
+
+  ngOnInit() {
+    this.meta.updateTag({ name: 'pragathpth', content: 'My Personal Portfolio' });
+  }
 }
